ff92be3c8712b336c56b004d5ef74fae33be2490
Zena Hadjivasiliou <zenah@pom-mac79141.biol.ucl.ac.uk>
2015-08-19 13:44:50 +0100

    zenas edits

--- src/InitiationFunctions.hpp
+++
@@ -1,5 +0,0 @@
-void  InitiateX(int array[N][2], double initial_p);
-
-void InitiateP(double mat[runs][2]);
-
-void IntiatepTraj(double mat[runs][maxSteps]);
--- src/WriteFiles.hpp
+++
@@ -1,6 +0,0 @@
-
-void WritePfix(double array[runs][2], int index);
-
-void WriteP_trajectory(double array[maxSteps]);
-
-void WriteHalflifes(int array[runs]);
